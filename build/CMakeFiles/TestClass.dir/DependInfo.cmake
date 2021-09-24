8710bde3fab55c3a2fdd669bc968fe503714c927
kowshicahmed <kowshicahmed@yahoo.com>
2021-09-24 09:20:31 +0200

    Testing CMake

--- build/CMakeFiles/TestClass.dir/DependInfo.cmake
+++ build/CMakeFiles/TestClass.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/kowshicahmed/WS_Project/weather_station/test/simple_test.cpp" "/home/kowshicahmed/WS_Project/weather_station/build/CMakeFiles/simple_test.dir/test/simple_test.cpp.o"
+  "/home/kowshicahmed/WS_Project/weather_station/lib/TestClass.cpp" "/home/kowshicahmed/WS_Project/weather_station/build/CMakeFiles/TestClass.dir/lib/TestClass.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -16,7 +16,6 @@ set(CMAKE_CXX_TARGET_INCLUDE_PATH
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/kowshicahmed/WS_Project/weather_station/build/CMakeFiles/TestClass.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
--- test/simple_test.cpp
+++ test/simple_test.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "TestClass.h"
+#include "modbus.h"
 
 int main()
 {
     std::cout << "Hello World" << std::endl;
+    TestClass my_num(10);
+    std::cout << my_num.get_item() << std::endl;
+    modbus_t *mb;
+    uint16_t tab_reg[2];
+
+    mb = modbus_new_rtu("/dev/ttyUSB0", 9600, 'N', 8, 1);
+    modbus_connect(mb);
+    if (mb == NULL)
+    {
+        fprintf(stderr, "Unable to create the libmodbus context\n");
+        return -1;
+    }
 }
