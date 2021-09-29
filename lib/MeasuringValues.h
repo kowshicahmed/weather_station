d95a4ecbc022c7a48034a4574b7c14fc8d3d5a75
kowshicahmed <kowshicahmed@yahoo.com>
2021-09-29 22:02:05 +0200

    Documenting MeasuringValues class

--- lib/MeasuringValues.h
+++ lib/MeasuringValues.h
@@ -13,20 +13,33 @@
  * Measuring Values (Input Register)
  *
  * All measuring values of the weather station WSC11 consume 32Bit, i.e. 2 MODBUS register addresses. 
- * This class reads the data from Input Registers for different parameters
+ * This class reads the data from Input Registers for different parameters.
+ * The data have a multiplication factor (10 or 100) associated with them.
  * The measuring values are sorted as follows:
- * - By measuring value type (30001 to 34999).
- * - In unbroken sequence (35001 to 39999).
+ * - By measuring value type (30001 to 34999)
+ * - In unbroken sequence (35001 to 39999)
  *
  */
-
 class MeasuringValues
 {
 private:
-    static constexpr int input_reg_start_addr = 3001;
-    static constexpr int wind_speed_addr = 30001;
+    static constexpr int input_reg_start_addr = 30001;      // Start adress of Input registers
+    static constexpr int wind_speed_addr = 30001;           // Wind speed, unit:m/s, mult: 10, Ex: 101 = 10.1m/s
+    static constexpr int mean_val_wind_speed_addr = 30003;
+    static constexpr int wind_direction_addr = 30201;
+    static constexpr int mean_val_wind_dir_addr = 30203;
     static constexpr int air_temp_addr = 30401;
+    static constexpr int interior_temp_addr = 30403;
+    static constexpr int rel_humidity_addr = 30601;
+    static constexpr int abs_humidity_addr = 30603;
+    static constexpr int dew_pt_temp_addr = 30605;
+    static constexpr int abs_air_press_addr = 30801;
+    static constexpr int rel_air_press_addr = 30803;
+    static constexpr int global_rad_addr = 31001;
+    static constexpr int brightness_north_addr = 31201;
+    static constexpr int brightness_east_addr = 31203;
     static constexpr int no_of_reg = 2;
+
     std::shared_ptr<modbus_t *> mb;
     uint16_t tab_reg[2];
 
--- lib/WeatherStation.cpp
+++ lib/WeatherStation.cpp
@@ -7,6 +7,9 @@
 
 #include "WeatherStation.h"
 
+/******************************* Constructors and Destructors *********************************************/
+
+/****** Constructor for creating new rtu connection with serial port ******************/
 WeatherStation::WeatherStation(std::string port_address_, int baud_rate_, char parity_, int data_bit_, int stop_bit_, int slave_id_)
     : port_address{port_address_}, baud_rate{baud_rate_}, parity{parity_}, data_bit{data_bit_}, stop_bit{stop_bit_}, slave_id{slave_id_}
 {
@@ -32,13 +35,14 @@ WeatherStation::WeatherStation(std::string port_address_, int baud_rate_, char p
         fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
         modbus_free(*mb);
     }
-    
+
     // Composition with the MeasuringValues class
     read_input_reg = std::make_shared<MeasuringValues>(mb);
 }
 
+/******************** Constructor for creating new tcp connection ****************/
 WeatherStation::WeatherStation(std::string ip_address_, int port_no_, int slave_id_)
-    : ip_address{ip_address_}, port_no {port_no_}, slave_id{slave_id_}
+    : ip_address{ip_address_}, port_no{port_no_}, slave_id{slave_id_}
 {
     // Converting port_address to char array
     int n = ip_address.length();
@@ -47,7 +51,7 @@ WeatherStation::WeatherStation(std::string ip_address_, int port_no_, int slave_
 
     // Creating new connection
     *mb = modbus_new_tcp(ip_address_char, port_no);
-    
+
     modbus_connect(*mb);
     if (mb == NULL)
     {
@@ -66,19 +70,29 @@ WeatherStation::WeatherStation(std::string ip_address_, int port_no_, int slave_
     read_input_reg = std::make_shared<MeasuringValues>(mb);
 }
 
+/***************** Copy Constructor *************************/
 WeatherStation::WeatherStation(const WeatherStation &source)
 {
     mb = std::make_shared<modbus_t *>();
     *mb = *source.mb;
 }
 
-
+/***************** Destructor *************************/
 WeatherStation::~WeatherStation()
 {
     modbus_close(*mb);
     modbus_free(*mb);
 }
 
+/******************************* Helper Functions *********************************************/
+
+/****** Returns composition pointer to the read data from input reg class********/
+std::shared_ptr<MeasuringValues> WeatherStation::readMeasuringValues()
+{
+    return read_input_reg;
+}
+
+/************** For testing purpose******************/
 void WeatherStation::read_software_version()
 {
     if (modbus_read_registers(*mb, 5004, 2, tab_reg) > 0)
@@ -101,9 +115,4 @@ void WeatherStation::read_relative_humidity()
         std::cout << tab_reg[0] << " " << tab_reg[1] << std::endl;
     else
         std::cout << "Error Reading data " << std::endl;
-}
-
-std::shared_ptr<MeasuringValues> WeatherStation::readMeasuringValues()
-{
-    return read_input_reg;
 }
